c41ff193fa91568ca16b43bdeec67caec0e9e33b
Sanya Doda <126786063+sanya-2304@users.noreply.github.com>
2025-04-11 00:38:21 +0530

    Update Login.jsx

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
         return handleError('Enter all fields before submitting.')
       } 
       try{
-        const url='http://localhost:5000/login'
+        const url='https://auth-full-stack-mern-api.vercel.app/login'
         const response=await fetch(url,{
           method:"POST",
           headers:{

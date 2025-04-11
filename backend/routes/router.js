0000d9b58fc233cf793809583f744ecd4855c6e8
Sanya Doda <126786063+sanya-2304@users.noreply.github.com>
2025-04-11 23:53:40 +0530

    updated router

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -5,6 +5,13 @@ const { loginValidation, signupValidation } = require('../middleware/validation'
 const router=require('express').Router()
 
 router.post('/login', loginValidation, login)
+router.get('/login', (req, res)=>{
+    res.send('login khul gya')
+})
+router.get('/signup', (req, res)=>{
+    res.send('signup khul gya')
+})
+
 router.post('/signup', signupValidation, signup)
 router.get('/home', auth, getData)
 

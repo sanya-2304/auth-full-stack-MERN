4ebf063d6a7aae9d8a9791e7beb46f0da75d47f2
Sanya Doda <126786063+sanya-2304@users.noreply.github.com>
2025-04-11 23:18:57 +0530

    added autofocus in login.jsx

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -66,7 +66,7 @@ const Login = () => {
             <input
               onChange={handleChange}
               type="email" 
-              name="email"
+              name="email" autoFocus
               placeholder="Enter your email"
               className="w-full px-4 py-2 text-gray-200 bg-gray-700 border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-400"
               value={LoginInfo.email}

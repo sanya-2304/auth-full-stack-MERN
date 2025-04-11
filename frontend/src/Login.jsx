61c3a1ab2a6aea5a416e18455ff99f475b402370
Sanya Doda <126786063+sanya-2304@users.noreply.github.com>
2025-04-11 23:40:08 +0530

    updated

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -54,7 +54,7 @@ const Login = () => {
     <div className="flex items-center justify-center min-h-screen bg-gray-900">
       <div className="bg-gray-800 p-8 rounded-2xl shadow-lg w-96">
         <h2 className="text-2xl font-semibold text-green-400 text-center mb-6">
-        Login
+        LogIn
         </h2>
         <form 
         onSubmit={handleSubmit}>
